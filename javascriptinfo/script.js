80543d1aba01f5fc3977a07e34019a5b92e3ab30
Noor Nile <aarondunkin123@gmail.com>
2023-01-22 19:35:46 -0500

    Added javascript into javascript.html and changed some appearences

---
+++ javascriptinfo/script.js
@@ -0,0 +1,10 @@
+const orangebutton = document.querySelector('orangebtn');
+orangebutton.addEventListener('click', turnOrange);
+const header = document.querySelector('.htmlheader')
+
+const newLocal = "background-color: orange;";
+orangebutton.style.cssText = newLocal;
+function turnOrange() {
+    
+    
+}
