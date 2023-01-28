c0b342071a2129edb701a91fe84e8684c9d78e7e
Noor Nile <aarondunkin123@gmail.com>
2023-01-27 20:28:52 -0500

    Added actual working javascript, but will need to fix the problem of changing color for only 1 box radius

--- javascriptinfo/script.js
+++ javascriptinfo/script.js
@@ -1,10 +1,14 @@
-const orangebutton = document.querySelector('orangebtn');
-orangebutton.addEventListener('click', turnOrange);
-const header = document.querySelector('.htmlheader')
+const orangebutton = document.querySelector('#orangebtn');
+const header = document.querySelector('.jsmain .htmlheader')
+const boxes = document.querySelector('.info-box1')
+const headerColor = document.querySelector(".jsmain .info-box1 h2");
 
-const newLocal = "background-color: orange;";
-orangebutton.style.cssText = newLocal;
+orangebutton.addEventListener('click', () => {
+    turnOrange();
+});
+
 function turnOrange() {
-    
-    
+    header.style.cssText = "background-color: rgb(255, 115, 0);";
+    boxes.style.cssText = "border-color: rgb(255, 115, 0);";
+    headerColor.style.cssText = "background-color: rgb(255, 115, 0);";
 }
